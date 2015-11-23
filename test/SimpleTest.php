f777062173981ddab10ff6fa69e0a49296d84895
Björn Schmitt <code@bjoern.io>
2015-11-23 23:52:20 +0100

    added test class

---
+++ test/SimpleTest.php
@@ -0,0 +1,14 @@
+<?php namespace Bschmitt\Amqp\Test;
+
+/**
+ * @author Björn Schmitt <code@bjoern.io>
+ */
+class SimpleTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testClassInstance()
+    {
+
+    }
+
+}
